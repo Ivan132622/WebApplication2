8c06088a7c765c76b036a83f94d07d1aaf07d514
Ivan <developer3@completespeech.com>
2021-05-10 14:55:17 -0600

    Agregar archivos de proyecto.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Mvc;
+
+
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult conditions()
+        {
+            return View();
+        }
+
+        public ActionResult use()
+        {
+            return View();
+        }
+
+        public ActionResult forum()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            //ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+       
+
+       
+    }
+}
